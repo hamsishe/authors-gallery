bbba72235e7cb383af5529fea6b113bdc2566227
hamsishe <hamsishe@gmail.com>
2020-12-29 15:41:33 +0200

    Update AuthorCard.js
    Remove useless conditions

--- src/Components/AuthorCard.js
+++ src/Components/AuthorCard.js
@@ -4,21 +4,11 @@ import { Card } from 'antd';
 const { Meta } = Card;
 
 function AuthorCard({ author }) {
-  const { urls, user } = author;
-  let description, alt_description;
-  if (author.description === null) {
-    description = 'No Decription';
-  } else {
-    description = author.description;
-  }
-  if (author.alt_description === null) {
-    alt_description = 'No Decription';
-  } else {
-    alt_description = author.alt_description;
-  }
+  const { urls, user, description, alt_description } = author;
+  
   return (
-    <Card className="authorCard" style={{ width: 240, height: 480 }} cover={<img src={urls.thumb} alt={description} />}>
-      <Meta title={user.username} description={alt_description} />
+    <Card className="authorCard" style={{ width: 240, height: 480 }} cover={<img src={urls.thumb} alt={description || 'No Decription'} />}>
+      <Meta title={user.username} description={alt_description || 'No Decription'} />
     </Card>
   );
 }
